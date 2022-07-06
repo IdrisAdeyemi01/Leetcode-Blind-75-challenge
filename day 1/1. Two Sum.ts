8e249981d1658286ff52ca8ec897bcdd726fc454
Iamsheye <seyepelumi@gmail.com>
2022-07-06 09:44:35 +0100

    restructure

--- day 1/1. Two Sum.ts
+++ day 1/1. Two Sum.ts
@@ -2,10 +2,10 @@ function twoSum(nums: number[], target: number): number[] {
   let map = new Map();
 
   for (let i = 0; i < nums.length; i++) {
-    let targetDiff = target - nums[i]
+    let targetDiff = target - nums[i];
     if (map.has(targetDiff)) {
-      return [map.get(target - nums[i]), i]
+      return [map.get(target - nums[i]), i];
     }
-    map.set(nums[i], iu
+    map.set(nums[i], i);
   }
-};
+}
